91e2467d6df305a134ef0e00ae01848d727901b4
David Peta <davidstefanpeta@gmail.com>
2019-05-25 11:10:40 +0300

    Lista Biserici

--- src/main/java/ro/esolutions/theRealPopeisComingApp/ConectorExtern.java
+++ src/main/java/ro/esolutions/theRealPopeisComingApp/ConectorExtern.java
@@ -3,18 +3,53 @@ package ro.esolutions.theRealPopeisComingApp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ConectorExtern {
 
     @GetMapping("/churches")
     public String getAllChurches(){
 
+        List<Biserica> biserici = new ArrayList<>();
+
         Biserica catedralaMantuiriiNeamului = new Biserica();
         catedralaMantuiriiNeamului.nume = "Catedrala Mantuirii Neamului";
         catedralaMantuiriiNeamului.tipReligie = "ortodoxa";
         catedralaMantuiriiNeamului.program = "10:00-19:00";
         catedralaMantuiriiNeamului.luna = "Mai";
-        return "Bisericile mele sunt: " + catedralaMantuiriiNeamului.nume;
 
+        Biserica catedralaSfIosif = new Biserica();
+        catedralaSfIosif.nume = "Catedrala Sf. Iosif";
+        catedralaSfIosif.tipReligie = "Catolica";
+        catedralaSfIosif.program = "09:00-20:00";
+        catedralaSfIosif.luna = "Noiembrie";
+
+        Biserica bunaVestireBelu = new Biserica();
+        bunaVestireBelu.nume = "Biserica Buna Vestire Belu";
+        bunaVestireBelu.tipReligie = "Ortodoxa";
+        bunaVestireBelu.program = "12:00-18:00";
+        bunaVestireBelu.luna = "Ianuarie";
+
+        Biserica moscheeaAlbastra = new Biserica();
+        moscheeaAlbastra.nume = "Moscheea Albastra";
+        moscheeaAlbastra.tipReligie = "musulmana";
+        moscheeaAlbastra.program = "24/7";
+        moscheeaAlbastra.luna = "Decembrie";
+
+        Biserica Balasa = new Biserica();
+        Balasa.nume = "Biserica Balasa";
+        Balasa.tipReligie = "adventista de ziua a saptea";
+        Balasa.program = "N/A";
+        Balasa.luna = "Aprilie";
+
+        biserici.add(catedralaMantuiriiNeamului);
+        biserici.add(catedralaSfIosif);
+        biserici.add(bunaVestireBelu);
+        biserici.add(moscheeaAlbastra);
+        biserici.add(Balasa);
+
+    return "Bisericile mele sunt: " + biserici. ;
     }
 }
