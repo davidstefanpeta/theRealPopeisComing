a14835fa3fae0088f39a3215b3755ac5abcbe787
David Peta <davidstefanpeta@gmail.com>
2019-05-25 10:25:16 +0300

    :Am adaugat aplicatie spring

---
+++ src/main/java/ro/esolutions/theRealPopeisComingApp/Biserica.java
@@ -0,0 +1,9 @@
+package ro.esolutions.theRealPopeisComingApp;
+
+public class Biserica {
+    String nume;
+    String tipReligie;
+    String program;
+    String luna;
+}
+
---
+++ src/main/java/ro/esolutions/theRealPopeisComingApp/TheRealPopeisComingAppApplication.java
@@ -0,0 +1,13 @@
+package ro.esolutions.theRealPopeisComingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRealPopeisComingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRealPopeisComingAppApplication.class, args);
+	}
+
+}
