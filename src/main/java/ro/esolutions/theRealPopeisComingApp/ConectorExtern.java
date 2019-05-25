a99c4509bd9a8198bba3433b2a8f4e72256e5a4b
David Peta <davidstefanpeta@gmail.com>
2019-05-25 10:48:12 +0300

    Am completat prima biserica

--- src/main/java/ro/esolutions/theRealPopeisComingApp/ConectorExtern.java
+++ src/main/java/ro/esolutions/theRealPopeisComingApp/ConectorExtern.java
@@ -8,6 +8,13 @@ public class ConectorExtern {
 
     @GetMapping("/churches")
     public String getAllChurches(){
-        return "Bisericile mele sunt: ";
+
+        Biserica catedralaMantuiriiNeamului = new Biserica();
+        catedralaMantuiriiNeamului.nume = "Catedrala Mantuirii Neamului";
+        catedralaMantuiriiNeamului.tipReligie = "ortodoxa";
+        catedralaMantuiriiNeamului.program = "10:00-19:00";
+        catedralaMantuiriiNeamului.luna = "Mai";
+        return "Bisericile mele sunt: " + catedralaMantuiriiNeamului.nume;
+
     }
 }
